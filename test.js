237b267bd2bbc8370e9de6a7c79cd56963611ec2
Glitch (level-humble-observatory) <none>
2021-04-20 16:35:18 +0000

    👵🔨 Checkpoint ./test.js:27597717/869

--- test.js
+++ test.js
@@ -2,4 +2,6 @@ const Sqlite = require('better-sqlite3');
 
 let db = new Sqlite('db.sqlite');
 
-console.log(db.prepare('SELECT * FROM question').all())
+console.log(new )
+// db.prepare('DROP TABLE IF EXISTS challenge').run();
+// db.prepare('CREATE TABLE challenge (id_match INTEGER PRIMARY KEY AUTOINCREMENT, challenger_id INTEGER, challenged_id INTEGER, id_set INTEGER, winner INTEGER, date TEXT)').run();
